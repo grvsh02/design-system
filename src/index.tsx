77443c30228546893f7c92d3185695bc8e710ecb
Gaurav <gaurav021201@gmail.com>
2022-08-12 01:21:31 +0530

    code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -14,5 +12,3 @@ root.render(
     </div>
   </React.StrictMode>
 );
-
-reportWebVitals();
