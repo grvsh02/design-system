472722a9d6e981f0aaac1b95068059ba3c4c023e
Gaurav <gaurav021201@gmail.com>
2022-08-14 18:00:19 +0530

    fixed outline buttons fill and stroke

--- src/icons/outline/Minus.tsx
+++ src/icons/outline/Minus.tsx
@@ -4,13 +4,11 @@ function SvgMinus(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
         d="M18 12.75H6c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h12c.41 0 .75.34.75.75s-.34.75-.75.75z"
-        fill="#4F4F4F"
       />
     </svg>
   );
--- src/icons/outline/Sort.tsx
+++ src/icons/outline/Sort.tsx
@@ -4,13 +4,11 @@ function SvgSort(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
         d="M21 7.75H3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h18c.41 0 .75.34.75.75s-.34.75-.75.75zM18 12.75H6c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h12c.41 0 .75.34.75.75s-.34.75-.75.75zM14 17.75h-4c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h4c.41 0 .75.34.75.75s-.34.75-.75.75z"
-        fill="#292D32"
       />
     </svg>
   );
