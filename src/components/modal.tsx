181a89122203e8b8d277665ba01c498f98b9f025
yakatyansh <ykpetrolhead08@outlook.com>
2022-09-10 11:33:03 +0530

    Modal done

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -17,25 +17,31 @@ const ModalContainer = styled('div')<ModalContainerProps>`
   display: ${props => props.isOpen? 'flex' : 'none'};
   justify-items: center;
   align-items: center;
+  justify-content: center;
   position: fixed; 
-  z-index: 1; 
+  z-index: 1;
   left: 0;
   top: 0;
   width: 100%; 
   height: 100%; 
   overflow: auto; 
   background-color: rgb(0,0,0); 
-  background-color: rgba(0,0,0,0.4); 
-`
+  background-color: rgba(0,0,0,0.4)
+`;
 
+const ModalCSS = styled('div')<ModalProps>`
+    margin: 15% ; /* 15% from the top and centered */
+    padding: 20px;
+    border : 1px solid #888;
+    background-color: #f4694c`;
 
 const Modal = ({children, isOpen, onClose}: ModalProps) => {
     return (
         <ModalContainer isOpen={isOpen}>
-            <Card>
-                <Button iconProps={{icon: "CloseCircle"}} onClick={onClose}/>
-                {children}
-            </Card>
+                <ModalCSS>
+                    <Button className="align-top align-right " iconProps={{icon: "CloseCircle",}}  size="base" onClick={onClose}/>
+                    {children}
+                </ModalCSS>
         </ModalContainer>
     )
 }
--- stories/modal.stories.tsx
+++ stories/modal.stories.tsx
@@ -23,7 +23,18 @@ const Template: Story = args => {
                 open
             </button>
             <Modal isOpen={isOpen}>
-                <h1>hello world</h1>
+                <h1>Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.
+                    Sed tempus consequat gravida.
+                    Quisque convallis iaculis aliquet.
+                    Pellentesque nec eros lorem.
+                    Nulla et sapien auctor, fermentum libero id, ultrices mi.
+                    Donec bibendum nibh quam, et volutpat ex aliquet eu. Duis id consectetur nunc.
+                    Vestibulum at aliquet elit, eget gravida nisi.
+                    Etiam sit amet lacus molestie, molestie ligula non, congue nunc. Morbi quam ante, porta non neque a, ultrices laoreet purus.
+                    Nunc ultricies augue est, id tincidunt justo porttitor vitae. Curabitur consequat urna non bibendum aliquam.
+
+                </h1>
             </Modal>
         </div>
     );
@@ -32,4 +43,6 @@ const Template: Story = args => {
 export const Default = Template.bind({});
 Default.args = {
     isOpen: false,
+    onClick: () => {},
+
 }
