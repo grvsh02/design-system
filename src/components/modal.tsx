ab6d891162047955a11406f091787eafaaf39a79
yakatyansh <ykpetrolhead08@outlook.com>
2022-09-11 10:51:47 +0530

    draft commit

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,20 +1,22 @@
-import React, {Children, ReactNode} from 'react';
+import React, {Children, ReactNode, useState} from 'react';
 import styled from "@emotion/styled";
 import Card from "./card";
 import Button from "./button";
+import * as open from "open";
 
 type ModalProps = {
     isOpen?: boolean;
     children?: ReactNode;
-    onClose?: () => void;
+    onClose?: () => {};
 }
+
 
 type ModalContainerProps = {
     isOpen?: boolean;
 }
 
 const ModalContainer = styled('div')<ModalContainerProps>`
-  display: ${props => props.isOpen? 'flex' : 'none'};
+  display: ${props => props.isOpen? 'block' : 'none'};
   justify-items: center;
   align-items: center;
   justify-content: center;
@@ -33,14 +35,24 @@ const ModalCSS = styled('div')<ModalProps>`
     margin: 15% ; /* 15% from the top and centered */
     padding: 20px;
     border : 1px solid #888;
-    background-color: #f4694c`;
+    background-color: #f4694c
+`;
 
-const Modal = ({children, isOpen, onClose}: ModalProps) => {
+
+
+const Modal = ({children,isOpen}: ModalProps) => {
+
+    const [isModalOpen, setIsModalOpen] = useState(isOpen);
+    const toggle = () => setIsModalOpen(!isModalOpen);
+
     return (
-        <ModalContainer isOpen={isOpen}>
+        <ModalContainer isOpen={isModalOpen}>
                 <ModalCSS>
-                    <Button className="align-top align-right " iconProps={{icon: "CloseCircle",}}  size="base" onClick={onClose}/>
-                    {children}
+                    <div>
+                        <h1 className="text-2xl font-bold">Are you sure you want to delete this item?</h1>
+                        {children}
+                    <Button className="" iconProps={{icon: "CloseCircle"}}  size="base" onClick={toggle}/>
+                    </div>
                 </ModalCSS>
         </ModalContainer>
     )
