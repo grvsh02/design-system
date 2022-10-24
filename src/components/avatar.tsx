84cce0fd2daf2446cba5e8a25c05f3c7c214f663
Gaurav Sharma <gaurav021201@gmail.com>
2022-10-24 16:38:29 +0530

    fix avatar

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,37 +1,68 @@
 import React from 'react';
-import styled from "@emotion/styled";
-import Icon from "./icon";
 
 type AvatarProps = {
     imgUrl?: string,
     size: 'base' | 'sm' | 'xs';
+    altText?: string;
+    onClick?: () => void;
+    altColors?: AvatarColorProps;
 }
 
-type AvatarContainerProps = {
-    size: 'base' | 'sm' | 'xs';
+type AvatarColorProps = {
+    fillColor?: string;
+    textColor?: string;
 }
 
-const AvatarContainer = styled('div')<AvatarContainerProps>`
-  width: ${props => props.size === 'base' ? '80px' : props.size === 'sm' ? '60px' : '30px'};
-  height: ${props => props.size === 'base' ? '80px' : props.size === 'sm' ? '60px' : '30px'};
-  img {
-    width: ${props => props.size === 'base' ? '80px' : props.size === 'sm' ? '60px' : '30px'};
-    height: ${props => props.size === 'base' ? '80px' : props.size === 'sm' ? '60px' : '30px'};
-    border-radius: 50%;
-  }
-`
+const Avatar = ({size = "base", imgUrl, altText, onClick = () => {}, altColors }: AvatarProps) => {
 
+    const getName = () => {
+        const _alt = (altText?.trim() || '');
+        const words = _alt.split(' ');
+        if (words.length > 1) return _alt[0] + words[words.length - 1][0];
+        else if (_alt.length > 1) return _alt[0] + _alt[1];
+        return _alt[0] || '';
+    };
 
-const Avatar = ({size = "base", imgUrl }: AvatarProps) => {
+    const iconSize = size === 'base' ? '96' : size === 'sm' ? '64px' : '40px';
+
+    const renderPlaceholder = () => (
+        <svg
+            className="rounded-full"
+            width={iconSize}
+            height={iconSize}
+            viewBox="0 0 75 75"
+            preserveAspectRatio="xMinYMid meet"
+            style={{ backgroundColor: altColors?.fillColor || '#ddd' }}
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+            <text
+                x="50%"
+                y="55%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                fill={altColors?.textColor || '#222'}
+                fontSize="32"
+                fontWeight="600"
+            >
+                {getName()?.toUpperCase()}
+            </text>
+        </svg>
+    );
+
+    const _className = (() => {
+        if(size === 'base') return 'w-24 h-24';
+        if(size === 'sm') return 'w-16 h-16';
+        if(size === 'xs') return 'w-10 h-10';
+    })();
 
     return (
-        <AvatarContainer size={size}>
-            {imgUrl ? (
-                <img src={imgUrl} alt="Not Found :_(" />
-            ): (
-                <Icon icon="Frame" type="Outline"/>
-            )}
-        </AvatarContainer>
+        <div className={`${_className}` + (typeof onClick === "function" ? ' hover:cursor-pointer' : '') } onClick={onClick}>
+            {!(imgUrl?.length) ?
+                renderPlaceholder()
+                : <img src={imgUrl} alt="Not Found :_(" className={ `${_className}` + " object-cover rounded-full "} />
+            }
+        </div>
     )
 }
 
--- stories/avatar.stories.tsx
+++ stories/avatar.stories.tsx
@@ -17,15 +17,25 @@ export default meta;
 
 const Template: Story = args => {
 
-        return (
-            <div>
-                <Avatar size={args.size} {...args}/>
-            </div>
-        );
+    return (
+        <div>
+            <Avatar size={args.size} {...args}/>
+        </div>
+    );
 }
 
 export const Default = Template.bind({});
 
 Default.args = {
-    imgUrl: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    imgUrl: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    altColours: {
+        fillColor: "#fde68ad9",
+        textColor: "#f59e0bd9",
+    },
+    altText:{
+        text: "Lorem Ipsum",
+    },
+    onClick: () => {
+        console.log("clicked");
+    }
 }
